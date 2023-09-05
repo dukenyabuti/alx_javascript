68a5bb11dddf4c830502f40f0624d3ebf9f8ead4
dukenyabuti <99273249+dukenyabuti@users.noreply.github.com>
2023-09-05 07:02:14 +0300

    JS

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,11 @@
+//Lets print three statements that praise languages
+let C = "C is fun";
+
+console.log(C);
+
+let Python = "Python is cool";
+
+console.log(Python)
+let JS = "JavaScript is amazing";
+
+console.log(JS);
