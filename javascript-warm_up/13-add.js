e5204488ae8c115053779255a31873ce83361f34
dukenyabuti <99273249+dukenyabuti@users.noreply.github.com>
2023-09-05 20:25:13 +0300

    JS

--- javascript-warm_up/13-add.js
+++ javascript-warm_up/13-add.js
@@ -1,8 +1,6 @@
+#!/usr/bin/node
 function add(num1, num2){
 return num1 + num2;
 
 }
-console.log(add(3, 5));
-console.log(add(-4, 85));
-console.log(add(93, -4));
-console.log(add(93, -4));
+console.log(add(num1, num2));
